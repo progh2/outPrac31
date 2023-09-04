31aedc4a593098c89cca16201d5ed79bfaacf095
Gihun Ham <ham@e-mirim.hs.kr>
2023-09-04 14:01:42 +0900

    초기 커밋.

---
+++ outPrac31/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace outPrac31
+{
+    internal class Program
+    {
+        static void NextPosition(int x, int y, int vx, int vy,
+            out int rx, out int ry)
+        {
+            rx = x + vx;
+            ry = y+vy;
+        }
+        static void Main(string[] args)
+        {
+            int x = 0; int y = 0;
+            int vx = 1; int vy = 1;
+
+            Console.WriteLine($"현재좌표: {x}, {y}");
+            NextPosition(x, y, vx, vy, out x, out y);
+            Console.WriteLine($"다음좌표: {x}, {y}");
+        }
+    }
+}
